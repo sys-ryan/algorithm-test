eb449a39261591d60c332fc55db895a6da42ec98
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-05 15:00:59 +0900

    재귀-연습2

---
+++ 0404/14888.py
@@ -0,0 +1,35 @@
+def calc(a, index, cur, plus, minus, mul, div):
+  # a: 입력으로 주어진 수열
+  # index: 현재 계산해야 하는 인덱스
+  # cur: index-1 번 까지 계산한 결과
+  # plus, minus, mul, div: 사용할 수 있는 연산자의 개수 
+
+  if index == len(a):
+    return (cur, cur)
+  
+  res = []
+  if plus > 0:
+    res.append(calc(a, index+1, cur+a[index], plus-1, minus, mul, div))
+  if minus > 0:
+    res.append(calc(a, index+1, cur-a[index], plus, minus-1, mul, div))
+  if mul > 0:
+    res.append(calc(a, index+1, cur*a[index], plus, minus, mul-1, div))
+  if div > 0:
+    if cur > 0:
+      res.append(calc(a, index+1, cur//a[index], plus, minus, mul, div-1))
+    else:
+      res.append(calc(a, index+1, -(cur//a[index]), plus, minus, mul, div-1))
+
+  ans = (
+    max([t[0] for t in res]),
+    min([t[1] for t in res])
+  )
+
+  return ans
+
+n = int(input())
+a = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+ans = calc(a, 1, a[0], plus, minus, mul, div)
+print(ans[0])
+print(ans[1])
---
+++ 0404/16197.py
@@ -0,0 +1,58 @@
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+n, m = map(int, input().split())
+a = [list(map(str, input())) for _ in range(n)]
+
+def go(step, x1, y1, x2, y2):
+  # step: button 누른 횟수
+  # (x1, y1), (x2, y2): 동전1 동전2 좌표 
+
+  if step == 11:
+    return -1
+  
+  fall1 = False
+  fall2 = False
+
+  if x1 < 0 or x1 >= n or y1 < 0 or y1 >= m:
+    fall1 = True
+  if x2 < 0 or x2 >= n or y2 < 0 or y2 >= m:
+    fall2 = True
+  
+  if fall1 and fall2:
+    return -1
+  if fall1 or fall2:
+    return step
+  
+  ans = -1
+  for k in range(4):
+    nx1, ny1 = x1+dx[k], y1+dy[k]
+    nx2, ny2 = x2+dx[k], y2+dy[k]
+
+    if 0 <= nx1 < n and 0 <= ny1 < m and a[nx1][ny1] == '#':
+      # 이동하려는 칸이 존재 하는데 & 그 칸이 벽인 경우 
+      nx1, ny1 = x1, y1
+    if 0 <= nx2 < n and 0 <= ny2 < m and a[nx2][ny2] == '#':
+      nx2, ny2 = x2, y2
+
+    temp = go(step+1, nx1, ny1, nx2, ny2)
+
+    if temp == -1:
+      continue
+    
+    if ans == -1 or ans > temp:
+      ans = temp
+  
+  return ans
+
+x1 = y1 = x2 = y2 = -1
+for i in range(n):
+  for j in range(m):
+    if a[i][j] == 'o':
+      if x1 == -1:
+        x1, y1 = i, j
+      else:
+        x2, y2 = i, j
+      a[i][j] = '.'
+
+print(go(0, x1, y1, x2, y2))
