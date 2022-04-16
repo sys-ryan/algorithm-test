a6a34f6b69e5c11fe3de0a1bc1f074a7eeb5df5d
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-17 01:55:44 +0900

    D&C

---
+++ 0417/1654.py
@@ -0,0 +1,22 @@
+# 길이 x로 자르면 몇 개의 랜선이 나오나?
+def check(a, m, x):
+  cnt = 0
+  for i in range(len(a)):
+    cnt += a[i] // x
+  return cnt >= m
+
+n, m = map(int, input().split())
+a = [int(input()) for _ in range(n)]
+ans = 0
+l = 1
+r = max(a)
+
+while l <= r:
+  mid = (l+r) // 2
+  if check(a, m, mid):
+    ans = max(ans, mid)
+    l = mid + 1
+  else:
+    r = mid - 1
+
+print(ans)
