7e917473d0a216d571487aaecde20f742cfbcd30
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-16 19:34:56 +0900

    D&C

---
+++ 0416/1790.py
@@ -0,0 +1,38 @@
+def calc(n):
+  ans = 0
+  start = 1
+  length = 1
+
+  while start <= n:
+    end = start * 10 - 1
+    if end > n:
+      end = n
+    ans += (end - start + 1) * length
+    start *= 10
+    length += 1
+  
+  return ans
+
+n, k = map(int, input().split())
+if calc(n) < k:
+  print(-1)
+  exit()
+
+left = 1
+right = n
+ans = 0
+
+while left <= right:
+  mid = (left + right) // 2
+  length = calc(mid)
+
+  if length < k:
+    left = mid + 1
+  else:
+    ans = mid
+    right = mid - 1
+
+s = str(ans)
+l = calc(ans)
+
+print(s[len(s) - (l-k) - 1])
