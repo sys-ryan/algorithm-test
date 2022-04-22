385df7908697ea4c03fc4724dfe64ce117763e42
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-23 02:54:06 +0900

    stack & queue

---
+++ 0421/17299.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+ans = [0] * n
+s = [0]
+
+freq = [0] * 1000001
+
+for i in range(n):
+  freq[a[i]] += 1
+
+for i in range(1, n):
+  if not s:
+    s.append(i)
+  while s and freq[a[s[-1]]] < freq[a[i]]:
+    ans[s.pop()] = a[i]
+  s.append(i)
+
+while s:
+  ans[s.pop()] = -1
+
+print(' '.join(map(str, ans)))
