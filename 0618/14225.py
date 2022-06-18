e719322dd47821ee88dedf4f8f5c270db6a126d8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-06-18 23:37:11 +0900

    brute force - recursion

---
+++ 0618/14225.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = list(map(int, input().split()))
+c = [False] * (n*100000 + 10)
+
+def go(i, s):
+  if i == n:
+    c[s] = True
+    return
+
+  go(i+1, s+a[i])
+  go(i+1, s)
+
+go(0, 0)
+
+i = 1
+while True:
+  if c[i] == False:
+    break
+  i += 1
+
+print(i)
